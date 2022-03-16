b64bcbabdbbd8a0de2d6f99290257f09d4295fce
RapNautico <foox299@gmail.com>
2022-03-16 16:47:27 -0500

    Se realizo modificaciones para saber crear una lista dentro de un diccionario y al reves tambien

--- diccionarios/lista_diccionario.py
+++ diccionarios/lista_diccionario.py
@@ -1,7 +1,27 @@
+#diccionario dentro de una lista
 estudiantes = [
     {'nombre': 'juan', 'edad':20},
     {'nombre': 'camila', 'edad':24}
 ]
 
-print(estudiantes)
+# print(estudiantes)
 
+clases = [estudiantes]
+
+for clase in clases:
+    print(clase)
+
+#lista dentro de un diccionario
+print("--------------------------------")
+estudiantes = []
+
+for numeroEstudiantes in range(5):
+    nuevoEstudiante = {'años': 20, 'nombre': 'Emanuel'}
+    estudiantes.append(nuevoEstudiante)
+#mostrar los primeros estudiantes
+for estudiante in estudiantes[:5]:
+    print(estudiante)
+print("--------------------------------")
+
+#ver la cantidad de estudiantes creados
+print(f"se han creado {str(len(estudiantes))} estudiantes ")
